fdfaf949955b1f8d5b5b2220ec12d57d3f5a2edc
AmoKiswaya <amosonganga254@gmail.com>
2026-02-12 12:08:33 +0300

    Implement add_expense method

--- src/expense_tracker_cli/expense_manager.py
+++ src/expense_tracker_cli/expense_manager.py
@@ -1,8 +1,34 @@
 from expense_tracker_cli.expense import Expense
 
 class ExpenseManager:
-    def add_expense(self):
-        pass
+    def __init__(self):
+        self._expenses = {}
+        self.load_expenses()
+
+    def expense_id_generate(self):
+        """
+        Create a unique expense ID based on existing expenses.
+        """    
+
+        if not self._expenses:
+            return 1
+        return max(self._expenses.keys()) + 1 
+    
+    def add_expense(self, amount, description):
+        try:
+            amount = float(amount)
+        except ValueError:
+            return "Invalid amount. Please enter a number" 
+
+        if amount <= 0:
+            return "Amount should be greater than 0"
+
+        new_id = self.expense_id_generate()
+        expense = Expense(new_id, description=description, amount=amount)
+        self._expenses[new_id] = expense
+        self.save_expenses()     
+        
+        return expense
 
     def list_expenses(Self):
         pass
@@ -13,4 +39,8 @@ class ExpenseManager:
     def delete_expense(self):
         pass
 
+    def save_expenses():
+        pass
     
+    def load_expenses():
+        pass
