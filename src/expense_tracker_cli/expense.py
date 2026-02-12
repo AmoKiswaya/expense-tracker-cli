1dd6523838572e40ddfdb414600d07743813faac
AmoKiswaya <amosonganga254@gmail.com>
2026-02-12 12:55:15 +0300

    Implement to_dict method

--- src/expense_tracker_cli/expense.py
+++ src/expense_tracker_cli/expense.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 
 class Expense:
-    def __init__(self, expense_id: int, description: str):
+    def __init__(self, expense_id: int, description: str, amount):
         """
         Create an expense object.
         expense_id: unique identifier
@@ -8,4 +10,16 @@ class Expense:
 
         self.id = expense_id
         self.description = description
-        
+        self.amount = amount
+        self.createdAt = f"{datetime.now():%d-%m-%YT%H:%M:%S}" 
+
+    def to_dict(self):
+        """
+        Convert expense object to a dictionary for JSON storage.
+        """
+        return {
+            "id": self.id,
+            "description": self.description,
+            "amount": self.amount,
+            "createdAt": self.createdAt
+        }
